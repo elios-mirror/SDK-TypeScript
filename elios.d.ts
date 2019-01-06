84afff26954879e2d12c57f4de21add9f754f5ae
hubert_i <leohub@live.fr>
2019-01-05 21:48:56 -0500

    First version

---
+++ elios.d.ts
@@ -0,0 +1,42 @@
+
+declare module 'elios' {
+
+    export interface Module {
+
+        /**
+         * Define what lowest version of the mirror your module need
+         */
+        readonly requireVersion: string;
+
+        /**
+         * Define if you want to show module in account loading message
+         */
+        readonly showOnStart: boolean;
+
+        /**
+         * Called when module was loaded
+         */
+        init: () => void;
+
+        /**
+         * Will be called when module need to be started
+         */
+        start: () => void;
+
+        /**
+         * Will be called when module need to be stopped
+         */
+        stop: () => void;
+    }
+
+
+    export interface Widget {
+        id: string;
+        html: BehaviorSubject<string>;
+    }
+
+    export interface Controller {
+        createWidget(args: {}): Widget;
+    }
+
+}
---
+++ rxjs.d.ts
@@ -0,0 +1,6 @@
+/**
+ * @class BehaviorSubject<T>
+ */
+declare class BehaviorSubject<T> {
+    next(value: T): void;
+}
